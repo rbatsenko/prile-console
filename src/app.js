9e742d87de3279c8ac9fa864e1fa774365586402
rbatsenko <rbatsenko@gmail.com>
2018-03-15 15:33:37 +0100

    bootstrap + icomoon

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'popper.js';
 import 'normalize.css/normalize.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));

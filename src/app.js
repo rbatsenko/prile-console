9c7a04a0d3c20a0364d7e22d7540023f95722cb4
rbatsenko <rbatsenko@gmail.com>
2018-03-15 15:38:30 +0100

    jquery + tether

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import 'tether';
 import 'popper.js';
 import 'normalize.css/normalize.css';
 import 'bootstrap';

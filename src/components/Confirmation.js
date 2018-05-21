eafe543f496b6cc5e4dca8c0a5b381cd96c88486
Jakub Staroń <staron.jk@gmail.com>
2018-05-21 23:38:04 +0200

    Fixes multiple requests in confimEmail.

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -2,29 +2,31 @@ import React from 'react';
 import $ from 'jquery';
 import axios from 'axios';
 
-const Confirmation = () => {
-    $('.app-wrap').css('opacity', '0');
-    $('body').css('background', 'white');
-    $('#app').hide();
-    /*$('.prile-loader').addClass('active');
-    setTimeout( () => {
-        $('.prile-loader').addClass('visible');
+export default class Confirmation extends React.Component {
+
+    componentDidMount() {
+        $('.app-wrap').css('opacity', '0');
+        $('body').css('background', 'white');
+        $('#app').hide();
+        /*$('.prile-loader').addClass('active');
         setTimeout( () => {
-            $('.prile-loader').addClass('animation');
-        }, 500);
-    }, 1000);*/
+            $('.prile-loader').addClass('visible');
+            setTimeout( () => {
+                $('.prile-loader').addClass('animation');
+            }, 500);
+        }, 1000);*/
 
-    let path = window.location.href.split( '/' );
-    console.log(path);
+        let path = window.location.href.split( '/' );
+        console.log(path);
 
-    axios.put('/accounts/actions/emailConfirmation', {
+        axios.put('/accounts/actions/emailConfirmation', {
                 confirmationPhrase: path[8],
                 email: path[6]
             },
             {
                 headers: { 'Content-Type': 'application/json' }
             }
-            )
+        )
             .then((response) => {
                 console.log(response);
                 if (response.status == '200') {
@@ -38,8 +40,11 @@ const Confirmation = () => {
                 console.log(error);
                 window.location.href = '/error';
             });
+    }
 
-    return null;
+    render() {
+        return (
+            <div></div>
+        );
+    }
 }
-
-export default Confirmation;

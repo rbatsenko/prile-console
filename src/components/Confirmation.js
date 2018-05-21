6a97ffe36f60275ae20c2720582ac082fa6c85ea
rbatsenko <rbatsenko@gmail.com>
2018-05-22 00:55:11 +0300

    confirmation fix

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -2,44 +2,49 @@ import React from 'react';
 import $ from 'jquery';
 import axios from 'axios';
 
-const Confirmation = () => {
-    $('.app-wrap').css('opacity', '0');
-    $('body').css('background', 'white');
-    $('#app').hide();
-    /*$('.prile-loader').addClass('active');
-    setTimeout( () => {
-        $('.prile-loader').addClass('visible');
+export default class Confirmation extends React.Component {
+
+    componentDidMount() {
+        $('.app-wrap').css('opacity', '0');
+        $('body').css('background', 'white');
+        $('#app').hide();
+        /*$('.prile-loader').addClass('active');
         setTimeout( () => {
-            $('.prile-loader').addClass('animation');
-        }, 500);
-    }, 1000);*/
+            $('.prile-loader').addClass('visible');
+            setTimeout( () => {
+                $('.prile-loader').addClass('animation');
+            }, 500);
+        }, 1000);*/
 
-    let path = window.location.href.split( '/' );
-    console.log(path);
+        let path = window.location.href.split( '/' );
+        console.log(path);
 
-    axios.put('/accounts/actions/emailConfirmation', {
-                confirmationPhrase: path[8],
-                email: path[6]
-            },
-            {
-                headers: { 'Content-Type': 'application/json' }
-            }
-            )
-            .then((response) => {
-                console.log(response);
-                if (response.status == '200') {
-                    window.location.href = '/success';
-                } else {
-                    console.log('Error!');
-                    window.location.href = '/error';
+        axios.put('/accounts/actions/emailConfirmation', {
+                    confirmationPhrase: path[8],
+                    email: path[6]
+                },
+                {
+                    headers: { 'Content-Type': 'application/json' }
                 }
-            })
-            .catch((error) => {
-                console.log(error);
-                window.location.href = '/error';
-            });
-
-    return null;
+                )
+                .then((response) => {
+                    console.log(response);
+                    if (response.status == '200') {
+                        window.location.href = '/success';
+                    } else {
+                        console.log('Error!');
+                        window.location.href = '/error';
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    window.location.href = '/error';
+                });
+    }
+    
+    render() {
+        return (
+            <div></div>
+        );
+    }
 }
-
-export default Confirmation;
